f7ac01f6c21fb69541a1ed13ddcc04b60bb7fab2
grguang <945455382@qq.com>
2020-11-24 15:26:38 +0800

    新增：     * 忽略组件文件夹的json配置；

--- scripts/compile.js
+++ scripts/compile.js
@@ -5,18 +5,21 @@
  * @flow
  */
 const externals = require("./externals");
-const compileComponent = require('@lugia/devtools-widgets');
+const compileComponent = require("@lugia/devtools-widgets");
+const componentInvalid = require("../src/widgets/invalidComponent.json");
 
 const commonInvalid = [];
-const componentInvalid = [];
 const hideInTollPanelComponents = {};
 const themeInvalid = [...commonInvalid];
 
 compileComponent({
-  externals, importModules: [{
-    libraryName: '@lugia/lugia-web',
-    libraryDirectory: 'dist',
-  }],
+  externals,
+  importModules: [
+    {
+      libraryName: "@lugia/lugia-web",
+      libraryDirectory: "dist"
+    }
+  ],
   componentInvalid,
   hideInTollPanelComponents,
   themeInvalid
